526dc4aabbe62eff05df0137c32e78be23f20643
nichuanfang <f18326186224@gmail.com>
2023-06-06 18:52:26 +0800

    ✨ feat: 查看Path.home()

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+# coding=utf-8
+
+from aligo import Aligo
+from loguru import logger
+from pathlib import Path
+
+# aligo = Aligo()
+
+logger.info(f'Path.home: {Path.home()}')
