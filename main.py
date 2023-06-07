810a9ffe5f4cf73ead1d80302d400a4404b4a516
nichuanfang <f18326186224@gmail.com>
2023-06-07 16:26:49 +0800

    ✨ feat: 默认覆盖原来的电影文件夹

--- main.py
+++ main.py
@@ -85,7 +85,9 @@ def crawl_movie(ali_drive:Alidrive):
                                     logger.success(f'电影:  {movie_name}刮削成功!')
                                     # 上传成功就将该文件夹移动到movies文件夹中 如果movies有同名文件夹 直接覆盖
                                     logger.info(f'开始移动tmm电影文件夹: {movie_folder.name}至movies')
-                                    ali_drive.aligo.move_file(file_id=movie_folder.file_id,to_parent_file_id=movies.file_id,overwrite=True)
+                                    res = ali_drive.aligo.move_file(file_id=movie_folder.file_id,to_parent_file_id=movies.file_id)
+                                    logger.info(f'res: {res}')
+                                    # ali_drive.aligo.move_file_to_trash(movie_folder.file_id)
                                     logger.success(f'tmm电影文件夹: {movie_folder.name}已成功移动至movies')
                                 else:
                                     logger.warning(f'电影:  {movie_name}刮削失败! 请检查电影文件名是否正确')
