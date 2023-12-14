4f388a4381bdb5040d760df1a4817aabb0db8864
f18326186224 <f18326186224@gmail.com>
2023-12-14 22:36:20 +0800

    log

--- auto_check_in.py
+++ auto_check_in.py
@@ -100,8 +100,11 @@ def prepare_for_aligo(base64_userdata:str):
         try:
             # 在一个月内 不再需要重新登录 直接使用上次登录的密钥
             refresh_token = aligo_config['refresh_token']
+            print(f'刷新前的refresh_token为{refresh_token}')
             # 使用refresh_token登录 如果刷新失败 直接抛异常AligoRefreshFailed
             aligo =  Aligo(refresh_token=refresh_token,re_login=False)
+            new_aligo_config:dict = json.loads(aligo_config_folder.read_text(encoding='utf8'))
+            print(f'刷新后的refresh_token为{new_aligo_config["refresh_token"]}')
             # 自动签到
             sign_in(refresh_token,bot)
             return aligo
